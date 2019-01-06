5d104ce205245223023c56214eaa8e271300f55a
oded282 <oded.bennoonshamir@live.biu.ac.il>
2019-01-06 19:06:36 +0200

    6.1 19:06

---
+++ FileCachManager.h
@@ -0,0 +1,15 @@
+#ifndef MILESTONE2_FILECACHMANAGER_H
+#define MILESTONE2_FILECACHMANAGER_H
+
+
+#include "Solver.h"
+
+class FileCachManager : public Solver {
+
+
+public:
+
+};
+
+
+#endif //MILESTONE2_FILECACHMANAGER_H
---
+++ Solver.h
@@ -0,0 +1,15 @@
+
+#ifndef MILESTONE2_SOLVER_H
+#define MILESTONE2_SOLVER_H
+
+class Solver{
+
+
+public:
+    virtual Solution solver(Problem) =0;
+};
+
+
+
+
+#endif //MILESTONE2_SOLVER_H
